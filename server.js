c8760c1ecebcf0536e51c627c4c50be10f78cf2f
Đào Văn Phương <121229016+PhuongCIT@users.noreply.github.com>
2024-10-30 22:07:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,13 @@ app.use(morgan("dev"));
 //ROUTES
 app.use("/api/v1/auth", require("./routes/userRoutes"));
 app.use("/api/v1/post", require("./routes/postRoutes"));
+//Home
+app.use("/",(req,res)=>{
+  req.status(200).send({
+    "success":true,
+    "msg":"node Server Running"
+  })
+})
 //PORT
 const PORT = process.env.PORT || 8080;
 
